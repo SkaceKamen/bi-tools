8f739ffd1a5aaa008637d77ba2a94f65a4ed3f57
Jan Zípek <jan@zipek.cz>
2024-02-14 09:23:36 +0100

    Add support for param/private variable declarations

--- packages/sqf-analyzer/src/analyzeSqf.ts
+++ packages/sqf-analyzer/src/analyzeSqf.ts
@@ -1,4 +1,14 @@
-import { SqfNode, SqfToken, walkSqf } from '@bi-tools/sqf-parser'
+import {
+	SqfLiteralNode,
+	SqfNode,
+	SqfToken,
+	walkSqf,
+} from '@bi-tools/sqf-parser'
+
+const isStringLiteral = (
+	node: SqfNode
+): node is SqfLiteralNode & { value: string } =>
+	node.type === 'literal' && typeof node.value === 'string'
 
 export const analyzeSqf = (
 	node: SqfNode,
@@ -29,29 +39,82 @@ export const analyzeSqf = (
 	}
 
 	walkSqf(node, (node) => {
-		if (node.type === 'assignment') {
-			let comment = undefined as string | undefined
-			const firstToken = sourceTokens.findIndex(
-				(token) => token.position.from === node.start
-			)
-			if (firstToken) {
-				const previousToken = sourceTokens[firstToken - 1]
-				if (
-					previousToken.type === 'line-comment' ||
-					previousToken.type === 'multi-line-comment'
-				) {
-					comment = previousToken.contents
+		switch (node.type) {
+			case 'assignment': {
+				let comment = undefined as string | undefined
+				const firstToken = sourceTokens.findIndex(
+					(token) => token.position.from === node.start
+				)
+				if (firstToken) {
+					const previousToken = sourceTokens[firstToken - 1]
+					if (
+						previousToken.type === 'line-comment' ||
+						previousToken.type === 'multi-line-comment'
+					) {
+						comment = previousToken.contents
+					}
 				}
+
+				const name = node.id.contents
+				getVariable(name).assignments.push({
+					position: [node.start, node.end],
+					comment,
+				})
+
+				break
 			}
 
-			const name = node.id.contents
-			getVariable(name).assignments.push({
-				position: [node.start, node.end],
-				comment,
-			})
-		} else if (node.type === 'variable') {
-			const name = node.id.contents
-			getVariable(name).usage.push([node.start, node.end])
+			case 'variable': {
+				const name = node.id.contents
+				getVariable(name).usage.push([node.start, node.end])
+
+				break
+			}
+
+			case 'binary-expression': {
+				switch (node.operator.contents) {
+					case 'params': {
+						if (node.right.type === 'array') {
+							for (const item of node.right.elements) {
+								if (isStringLiteral(item)) {
+									const name = item.value
+									getVariable(name).assignments.push({
+										position: [item.start, item.end],
+										comment: undefined,
+									})
+								} else if (item.type === 'array') {
+									const firstItem = item.elements[0]
+									if (isStringLiteral(firstItem)) {
+										const name = firstItem.value
+										getVariable(name).assignments.push({
+											position: [firstItem.start, firstItem.end],
+											comment: undefined,
+										})
+									}
+								}
+							}
+						}
+
+						break
+					}
+
+					case 'private': {
+						if (node.right.type === 'array') {
+							for (const item of node.right.elements) {
+								if (isStringLiteral(item)) {
+									const name = item.value
+									getVariable(name).assignments.push({
+										position: [item.start, item.end],
+										comment: undefined,
+									})
+								}
+							}
+						}
+
+						break
+					}
+				}
+			}
 		}
 	})
 
